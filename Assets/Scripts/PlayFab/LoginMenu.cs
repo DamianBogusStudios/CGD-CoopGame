92927ab248dce1a474338606eab2603b28afad4f
b1uto <112058075+b1uto@users.noreply.github.com>
2024-03-24 00:57:40 +0000

    Updated Login
    ___Guestlogin,RegisterMenu

--- Assets/Scripts/PlayFab/LoginMenu.cs
+++ Assets/Scripts/PlayFab/LoginMenu.cs
@@ -9,9 +9,11 @@ public class LoginMenu : MonoBehaviour
 {
     [Header("UI")]
     public TMP_Text Message;
-    public TMP_InputField Email;
+    public TMP_InputField ID;
     public TMP_InputField Password;
+    public TMP_InputField UserName;
 
+<<<<<<< Updated upstream
     // Login Authentication
     public void LoginUser()
     {
@@ -29,11 +31,33 @@ public class LoginMenu : MonoBehaviour
 
     // For Login with email
     void LoginWithEmail(string email, string password)
+=======
+    public void LoginUser()
+    {
+        string email = ID.text;
+        string password = Password.text;
+        if (IsValidEmail(email))
+        {
+            Login(email, password);
+        }
+        else
+        {
+            LoginUsername(password);
+        }
+    }
+
+    //For Login
+     void Login(string email, string password)
+>>>>>>> Stashed changes
     {
         var request = new LoginWithEmailAddressRequest
         {
             Email = email,
+<<<<<<< Updated upstream
             Password = password
+=======
+            Password = password,
+>>>>>>> Stashed changes
         };
         PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
     }
@@ -57,13 +81,32 @@ public class LoginMenu : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+<<<<<<< Updated upstream
     // Reset Password
+=======
+    public void LoginUsername(string password)
+    {
+        var request = new LoginWithPlayFabRequest
+        {
+            Username = UserName.text,
+            Password = password,
+        };
+        PlayFabClientAPI.LoginWithPlayFab(request, OnLoginSuccess, OnError);
+    }
+    
+    //Reset Password
+>>>>>>> Stashed changes
     public void ResetPassword()
     {
         var request = new SendAccountRecoveryEmailRequest
         {
+<<<<<<< Updated upstream
             Email = Email.text,
             TitleId = "YOUR_PLAYFAB_TITLE_ID"
+=======
+            Email = ID.text,
+            TitleId = "91BDE",
+>>>>>>> Stashed changes
         };
         PlayFabClientAPI.SendAccountRecoveryEmail(request, OnPasswordReset, OnError);
     }
@@ -113,5 +156,35 @@ public class LoginMenu : MonoBehaviour
     {
         Debug.LogError("Error: " + error.GenerateErrorReport());
         Message.text = "Error: " + error.ErrorMessage;
+    }
+
+    //tocheckthe email
+    bool IsValidEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public void OnGuestLogin()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true,
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnGuestLoginSuccess, OnError);
+    }
+
+    void OnGuestLoginSuccess(LoginResult result)
+    {
+        Message.text = "Login Successful";
+        SceneManager.LoadScene(1);
     }
 }
